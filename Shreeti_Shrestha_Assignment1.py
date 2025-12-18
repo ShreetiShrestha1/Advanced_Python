976018b349a774687459c106ad15a1beb8931edb
ShreetiShrestha1 <shreetishrestha94@gmail.com>
2025-12-18 21:11:56 +0545

    updated assignment 1

---
+++ Shreeti_Shrestha_Assignment1.py
@@ -0,0 +1,37 @@
+#1
+my_age=20
+print("My age is",my_age)
+
+#2
+food= input("What is your favourite food:")
+print("Your favourite food is",food)
+
+#3
+num_str ="42"
+num= int(num_str)
+print(num)
+print(type(num))
+
+#4
+num_float=3.14159
+num=str(num_float)
+print(num)
+print(type(num))
+
+
+#5
+str1 = "Hello"
+str2 = "World"
+str3 = str1 + " " + str2
+print(str3)
+
+#6
+string = "Python"
+print(string[2])
+
+#7
+a = input("Enter a string:")
+first_five = a[0:5]
+print("The first 5 words are:", first_five)
+
+
